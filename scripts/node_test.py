859e732bc4e9e2a74a8574fc3e8ec75dcd3eefac
Michael Costa <macosta@inf.ufpel.edu.br>
2018-12-17 21:41:43 -0200

    fixed the code to use ROS and follow me
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- scripts/node_test.py
+++ scripts/node_test.py
@@ -6,65 +6,83 @@ from std_msgs.msg import String
 
 leftValue = 0
 rightValue = 0
+state = 'stop'
 
 def turnRight():
-    global leftValue
     try:
         startMovimentTurnRight()
-    except rospy.ROSInterruptException:
-        pass
-    while not leftValue >= 953:
-        rospy.spin()
-    try:
-        stopMoviment()
-    except rospy.ROSInterruptException:
+    except rospy.ROSInnterruptException:
         pass
 
 def turnLeft():
-    global rightValue
     try:
         startMovimentTurnLeft()
-    except rospy.ROSInterruptException:
+    except rospy.ROSInnterruptException:
         pass
-    while not rightValue >= 953:
-        rospy.spin()
-    try:
-        stopMoviment()
-    except rospy.ROSInterruptException:
-        pass
-
-def stopGoAhead(value):
-    return (value/1200.0)*3.14*0.34 >= 1.0
 
 def goAhead():
-    global leftValue
-    global rightValue
     try:
         startMovimentAhead()
-    except rospy.ROSInterruptException:
+    except rospy.ROSInnterruptException:
         pass
-    while not (stopGoAhead(rightValue) or stopGoAhead(leftValue)):
-        rospy.spin()
+
+def followMe():
     try:
-        stopMoviment()
-    except rospy.ROSInterruptException:
+        None
+    except rospy.ROSInnterruptException:
+        pass
+
+def stopMoviment():
+    try:
+        stop()
+    except rospy.ROSInnterruptException:
         pass
 
 def callbackLeft(data):
     global leftValue
     leftValue = int(data.data)
+    evaluateStop()
 
 def callbackRight(data):
     global rightValue
     rightValue = int(data.data)
+    evaluateStop()
 
 def callbackWalk(data):
-    if (data.data == 'goAhead'):
+    global state
+    if data.data == 'go ahead':
+        state = 'goAhead'
         goAhead()
-    if (data.data == 'turnLeft'):
+    elif data.data == 'turn left':
+        state = 'turnLeft'
         turnLeft()
-    if (data.data == 'turnRight'):
+    elif data.data == 'turn right':
+        state = 'turnRight'
         turnRight()
+    elif data.data == 'follow me':
+        state = 'followMe'
+        followMe()
+    elif data.data == 'stop':
+        state = 'stop'
+        stopMoviment()
+
+def stopGoAhead(value, distance):
+    return (value/1200.0)*3.14*0.34 >= distance
+
+def evaluateStop():
+    global state
+    global leftValue
+    global rightValue
+    if state == 'stop':
+        stopMoviment()
+    elif state == 'turnRight' and leftValue >= 400:
+        stopMoviment()
+    elif state == 'turnLeft' and rightValue >= 400:
+        stopMoviment()
+    elif state == 'goAhead' and (stopGoAhead(rightValue, 1.0) or stopGoAhead(leftValue, 1.0)):
+        stopMoviment()
+    elif state == 'followMe':
+        None
 
 def startMovimentTurnRight():
     pub = rospy.Publisher('pattern', Bool, queue_size=1)
@@ -76,7 +94,7 @@ def startMovimentTurnLeft():
     pub = rospy.Publisher('pattern', Bool, queue_size=1)
     pub.publish(True)
     move = rospy.Publisher('channel_x', Int16, queue_size=1)
-    move.publish(175)
+    move.publish(160)
 
 def startMovimentAhead():
     pub = rospy.Publisher('pattern', Bool, queue_size=1)
@@ -84,11 +102,13 @@ def startMovimentAhead():
     move = rospy.Publisher('channel_y', Int16, queue_size=1)
     move.publish(175)
 
-def stopMoviment():
+def stop():
     pub = rospy.Publisher('pattern', Bool, queue_size=1)
     pub.publish(True)
-    stop = rospy.Publisher('channel_y', Int16, queue_size=1)
-    stop.publish(135)
+    stopy = rospy.Publisher('channel_y', Int16, queue_size=1)
+    stopy.publish(135)
+    stopx = rospy.Publisher('channel_x', Int16, queue_size=1)
+    stopx.publish(135)
 
 def listener():
     rospy.Subscriber("left_sensor", Int16, callbackLeft)
